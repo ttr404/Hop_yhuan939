aa5f2c3ded56a7dbf7d06cdebaa387702c58a47f
yhuan939 <yhuan939@uwo.ca>
2023-11-17 22:02:23 +0000

    vc

--- Router.cpp
+++ Router.cpp
@@ -1,20 +1,5 @@
 #include "Router.h"
 
-/**
- * @brief write_callback writes data and re
- *
- * @param ptr
- * @param size
- * @param nmemb
- * @param data
- * @return size_t
- */
-size_t write_callback(char *ptr, size_t size, size_t nmemb, std::string *data)
-{
-    data->append(ptr, size * nmemb);
-    return size * nmemb;
-}
-
 Router::Router() : app(app)
 {
 }
@@ -43,7 +28,6 @@ int Router::enroute(crow::SimpleApp &app)
              ctx["description"] = "HOP.cheap: Shop without Search, hop in shopping experience.";
              ctx["year"] = std::localtime(&now)->tm_year + 1900;
              return page.render(ctx); });
-
 
         CROW_ROUTE(app, "/favicon.ico")
         ([]
@@ -78,52 +62,21 @@ int Router::enroute(crow::SimpleApp &app)
             auto response = page.render(ctx);
             return crow::response(response); });
 
+        CROW_ROUTE(app, "/openai/<string>")
+        ([&](std::string query)
+         { return crow::response(api.response_openAI(query)); });
+
         CROW_ROUTE(app, "/suggestion")
-        ([]()
-         {
-        std::string url = "https://trends.google.com/trends/trendingsearches/daily/rss?geo=CA";
-        CURL *curl;
-        CURLcode res;
-        std::string response;
-        curl = curl_easy_init();
-        if (curl)
-        {
-            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-            res = curl_easy_perform(curl);
-            curl_easy_cleanup(curl);
-        }
-        return crow::response(response); });
+        ([&]()
+         { return crow::response(api.googleTrends()); });
 
         CROW_ROUTE(app, "/suggestion/<string>")
-        ([](std::string query)
-         {
-            std::string url = "https://www.bing.com/osjson.aspx?query=" + query;
-            CURL *curl;
-            CURLcode res;
-            std::string response;
-            curl = curl_easy_init();
-            if (curl)
-            {
-                curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-                curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-                res = curl_easy_perform(curl);
-                curl_easy_cleanup(curl);
-            }
-            return crow::response(response); });
-
-        
+        ([&](std::string query)
+         { return crow::response(api.bingSuggestion(query)); });
 
         CROW_ROUTE(app, "/db")
-        ([]()
-         {
-            Database db;
-            db.connect();
-            return db.getAll(); });
+        ([&]()
+         { return db.getAll(); });
     }
     catch (const std::exception &e)
     {
@@ -132,6 +85,6 @@ int Router::enroute(crow::SimpleApp &app)
     return 0;
 }
 
-crow::json::wvalue handleQuery(std::string query)
+std::string Router::handleQuery(std::string query)
 {
 }
