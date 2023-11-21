85ffc621d8dd2bf0d482fb048edd42dab558db54
hxia47 <hxia47@uwo.ca>
2023-11-21 22:36:08 +0000

    finally finished the voice input part :)

--- static/js/voice.js
+++ static/js/voice.js
@@ -4,64 +4,78 @@
 // it just feels like i am trying to fit a square peg into a round hole
 // so i am pretty much stuck with js for now
 
-
-const voiceInput = () => {
-  // check if the browser supports the MediaDevices API
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    console.error("MediaDevices API or getUserMedia method not supported.");
-    return Promise.reject(new Error('Get a proper browser dude.'));;
-  }
-
-
-  let mediaRecorder;
-  let audioChunks = [];
-  let isRecording = false;
-  
-  // const player = document.getElementById('player');
-  const recordButton = document.getElementById('recordButton');
-  
-  // event listener for the start and stop buttons
-  recordButton.addEventListener('click', toggleRecording);
-  
-  // toggle recording
-  function toggleRecording() {
-    // change the icon
-    const icon = recordButton.querySelector('i'); 
-    if (isRecording) {
-        // stop recording
-        mediaRecorder.stop();
-        recordButton.innerHTML = '<i class="ri-send-plane-fill"></i>';
-      } else {
-        // start recording
-        audioChunks = [];
-        mediaRecorder.start();
-        recordButton.innerHTML = '<i class="ri-stop-circle-fill"></i>';
-    }
-    isRecording = !isRecording;
-  }
-  
-  // request microphone access
-  navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-          mediaRecorder = new MediaRecorder(stream);
-          mediaRecorder.ondataavailable = event => {
-              audioChunks.push(event.data);
-          };
-  
-          mediaRecorder.onstop = () => {
-              const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
-              const audioUrl = URL.createObjectURL(audioBlob);
-              // player.src = audioUrl;
-          };
-      })
-      .catch(error => console.error('Error accessing the microphone:', error));
-  
-  
-
-
-  return (
-    
-  )
+// check if the browser supports the MediaDevices API
+if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+  console.error("MediaDevices API or getUserMedia method not supported.");
+  Promise.reject(new Error('Get a proper browser dude.'));;
 }
 
-export default voiceInput;
+let mediaRecorder;
+let audioChunks = [];
+let isRecording = false;
+
+async function getAudio() {
+  console.log('Requesting microphone access...');
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.ondataavailable = event => {
+      audioChunks.push(event.data);
+    };
+    mediaRecorder.onstop = () => {
+      try {
+          const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
+          const audioUrl = URL.createObjectURL(audioBlob);
+          const recordedAudio = document.createElement("audio");
+          recordedAudio.src = audioUrl;
+          console.log('audioBlob:', audioBlob);
+          console.log('audioChunks:', audioChunks);
+          console.log('audioUrl:', audioUrl);
+          console.log('recordedAudio:', recordedAudio);
+      } catch (error) {
+          console.error('Error in onstop:', error);
+      }
+    };
+    
+  } catch (error) {
+    console.error('Error accessing the microphone:', error);
+    throw error; // Re-throw the error to be handled in toggleRecording
+  }
+}
+
+// const player = document.getElementById('player');
+const recordButton = document.getElementById('recordButton');
+
+// event listener for the start and stop buttons
+recordButton.addEventListener('click', toggleRecording);
+
+// toggle recording
+async function toggleRecording() {
+  console.log('toggleRecording()');
+  if (isRecording) {
+    console.log('stopping recording');
+    // stop recording
+    try {
+      await mediaRecorder.stop();
+      isRecording = false;
+      // swap between the icon
+      document.getElementById('recordButton').innerHTML = '<i class="ri-mic-fill"></i>';
+    } catch (error) {
+      console.error('Error stopping recording:', error);
+    }
+
+  } else {
+    console.log('start recording');
+    // start recording
+    audioChunks = [];
+    try {
+      await getAudio();
+      console.log('mediaRecorder:', mediaRecorder);
+      mediaRecorder.start();
+      document.getElementById('recordButton').innerHTML = '<i class="ri-stop-circle-fill"></i>';
+      isRecording = true;
+    } catch (error) {
+      console.error('Error starting recording:', error);
+    }
+  }
+}
