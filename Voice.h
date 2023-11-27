bf728f3424918762ef79cdc10e63a9427cda52c3
jding263 <jding263@uwo.ca>
2023-11-27 22:25:41 +0000

    feat: Update voice class with base64 encoding

--- Voice.h
+++ Voice.h
@@ -1,18 +1,20 @@
 #ifndef VOICE_H
 #define VOICE_H
 
-#include <curl/curl.h>
+#include <boost/archive/iterators/binary_from_base64.hpp>
+#include <boost/archive/iterators/transform_width.hpp>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
+#include <map>
 
 class Voice {
-private:
-
 public:
-    void Voice::convertBlob(std::string blobPath);
-    void Voice::sendToReplicate(std::string blob);
+    Voice() {};
+    void upload(std::string& body, const std::string& file_path);
+    std::map<std::string, std::string> parse_form_data(const std::string& body);
+    void decode_base64_and_write_to_file(const std::string& base64, const std::string& file_path);
 };
-
-
-
 
 #endif
