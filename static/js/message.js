88a047bd9e66fa746243ea94ece619202c1bf4b7
yhuan939 <yhuan939@uwo.ca>
2023-11-28 23:47:05 +0000

    fix bugs on upload fail

--- static/js/message.js
+++ static/js/message.js
@@ -5,24 +5,28 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault(); // Prevent the default form submission
 
         var formData = new FormData(uploadForm);
-        var xhr = new XMLHttpRequest();
 
-        xhr.open('POST', '/uploadImage', true);
-
-        xhr.onload = function () {
-            if (this.status === 200) {
-                // If the request was successful, display the success message
-                var successMessage = document.createElement('p');
-                successMessage.textContent = 'Upload successful!';
-                uploadForm.appendChild(successMessage);
+        fetch('/uploadImage', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => {
+            if (response.ok) {
+                return response.text();
             } else {
-                // If the request failed, display an error message
-                var errorMessage = document.createElement('p');
-                errorMessage.textContent = 'Upload failed. Please try again.';
-                uploadForm.appendChild(errorMessage);
+                throw new Error('Network response was not ok.');
             }
-        };
-
-        xhr.send(formData);
+        })
+        .then(text => {
+            var successMessage = document.createElement('p');
+            successMessage.textContent = 'Upload successful!';
+            uploadForm.appendChild(successMessage);
+        })
+        .catch(error => {
+            var errorMessage = document.createElement('p');
+            errorMessage.textContent = 'Upload failed. Please try again.';
+            uploadForm.appendChild(errorMessage);
+            console.error('There has been a problem with your fetch operation:', error);
+        });
     });
 });
